0f89487c84ef1b106ee30eb5d567f3acc9aaff5e
daniapraezss <daniapraez@hotmail.com>
2021-08-17 13:26:37 -0500

    Update

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,9 @@ const cors = require("cors");
 const { dbConnection } = require("./db/db");
 const Role = require("./routes/role");
 const User = require("./routes/user");
+const Auth = require("./routes/auth");
+const Board = require("./routes/board");
+
 
 require("dotenv").config();
 
@@ -13,6 +16,8 @@ app.use(express.json());
 app.use(cors());
 app.use("/api/role", Role);
 app.use("/api/user", User);
+app.use("/api/auth", Auth);
+app.use("/api/board", Board);
 
 app.listen(process.env.PORT, () => console.log("BackEnd server running on port: ", process.env.PORT));
 
